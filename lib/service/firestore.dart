c0f7cf2c881c27f84edc89ccb3ddb5cdef54c098
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2022-05-30 06:29:13 +0900

    createPost, getPostListを作成

--- lib/service/firestore.dart
+++ lib/service/firestore.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:myhands/model/User.dart';
 import 'package:myhands/model/Profile.dart';
 import 'package:myhands/model/Post.dart';
+import 'package:myhands/ui/component/trump_card.dart';
 
 class FirestoreService {
   static Future<Profile> getUserProfile(String uid) async {
@@ -12,5 +12,28 @@ class FirestoreService {
 
     Profile profile = Profile(profileSnapshot['uid'], profileSnapshot['username'], profileSnapshot['nickname'], profileSnapshot['profile'], profileSnapshot['birthday'].toDate());
     return profile;
+  }
+
+  static Future<List<Post>> getPostList(String uid) async {
+    List<Post> post_list = [];
+    final postCollectionSnapshots = await FirebaseFirestore.instance
+    .collection('users')
+    .doc(uid)
+    .collection('posts')
+    .get();
+
+    for (var doc in postCollectionSnapshots.docs) {
+      post_list.add(Post(uid, doc.id, doc["title"], doc["description"], [], [], TrumpCard(rank: "", suit: "",), TrumpCard(rank: "", suit: "",), "", "", "", ""));
+    }
+    return post_list;
+  }
+
+  static createPost(Post post) async {
+    await FirebaseFirestore.instance
+    .collection('users')
+    .doc(post.uid)
+    .collection('posts')
+    .doc()
+    .set(post.toJson());
   }
 }
