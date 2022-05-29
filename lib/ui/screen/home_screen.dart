76b687873846085746238d8d71016309e7ace07e
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2022-05-30 06:30:22 +0900

    HomeScreen

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:myhands/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myhands/ui/view/edit.dart';
+import 'package:myhands/ui/view/post_list.dart';
 
 // [Themelist] インスタンスにおける処理。
 class HomeScreen extends StatelessWidget {
@@ -13,17 +15,54 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = L10n.of(context)!;
     return Scaffold(
-      // メイン画面
-      body: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
-            child: Text(l10n.ui__home__title,
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
-        ]),
-      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0, top: 20.0, left: 20.0, right: 10.0),
+              child: Container(
+                width: double.infinity,
+                child: Row(children: [
+                  const Text(
+                    'あなたのメモ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 36,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute<void>(
+                        builder: (BuildContext context) {
+                          return Edit();
+                        },
+                      ));
+                    },
+                    label: const Text('新規メモ'),
+                    icon: const Icon(Icons.edit),
+                  )
+                ]),
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 0.0, top: 10.0, left: 20.0, right: 20.0),
+                child: Container(
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: PostList(),
+                )
+              )
+            )
+          ],
+        ),
+      )
     );
   }
 }
