1cfc43bcdfafb7f5b57c58a3b1bcd74a0e168a6f
IAMKOTARO <saitou.kotarou898@mail.kyutech.jp>
2022-05-30 06:28:52 +0900

    Postオブジェクトを修正

--- lib/model/Post.dart
+++ lib/model/Post.dart
@@ -1,13 +1,41 @@
+import 'package:myhands/ui/component/trump_card.dart';
+
 class Post {
   String uid = "";
-  String content = "";
+  String post_id = "";
 
-  Post(this.uid, this.content);
+  String title = "";
+  String description = "";
+  List<TrumpCard> hero = [];
+  List<TrumpCard> frop = [];
+  TrumpCard turn = TrumpCard(rank: "", suit: "");
+  TrumpCard river = TrumpCard(rank: "", suit: "");
+  String hero_memo = "";
+  String frop_memo = "";
+  String turn_memo = "";
+  String river_memo = "";
+
+  Post(this.uid, this.post_id, this.title, this.description, this.hero, this.frop, this.turn, this.river, this.hero_memo, this.frop_memo, this.turn_memo, this.river_memo);
 
   toJson() {
     return {
       "uid": uid,
-      "content": content,
+      "title": title,
+      "description": description,
+      "hero": [
+        for (int i = 0; i < hero.length; i++)
+          hero[i].toJson()
+      ],
+      "frop": [
+        for (int i = 0; i < frop.length; i++)
+          frop[i].toJson()
+      ],
+      "turn": turn.toJson(),
+      "river": river.toJson(),
+      "hero_memo": hero_memo,
+      "frop_memo": frop_memo,
+      "turn_memo": turn_memo,
+      "river_memo": river_memo,
     };
   }
 }
